8dd52ae8d13f34df2c8385c8be9eabd4b86b7aea
nicolaciar <nicolas.laciar@mi.unc.edu.ar>
2025-08-14 19:16:28 -0300

    Implementa la función actualizarLista

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ function agregarAmigo() {
     // Obtenemos el nombre del amigo ingresado en la caja de texto
     let input = document.getElementById("amigo");
     let amigo = input.value;
+
     // Verifica que la cadena no esté vacía
     if (amigo.trim().length === 0) {
         alert("Ingrese un nombre válido");
@@ -15,7 +16,23 @@ function agregarAmigo() {
     // Si no lo está, agrega el amigo a la lista
     else {
         amigos.push(amigo.trim());
+        // Actualiza la lista de amigos en pantalla
+        actualizarLista();
     }
+
     // Limpia la caja de texto
     input.value = "";
+}
+
+function actualizarLista() {
+    let ulAmigos = document.getElementById("listaAmigos");
+    ulAmigos.innerHTML = ""; // Limpia la lista actual
+
+    //Recorremos el array amigos y agregamos los items il
+    for (let amigo of amigos){
+        let liAmigo = document.createElement("li");
+        liAmigo.textContent = amigo;
+        ulAmigos.appendChild(liAmigo);
+    }
+
 }
